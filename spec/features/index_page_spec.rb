9f77f35dc32b12fae45ec2dac29183d5e8b0de84
count_xyz <skeptoid@gmail.com>
2015-03-28 16:08:06 -0400

    Back-write title spec

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 RSpec.describe 'Index Page' do
   before { visit '/' }
 
+  it "has title 'D3 Resume'" do
+    expect(page).to have_title 'D3 Resume'
+  end
+
   it "has h1 with 'Efren Aguirre'" do
     expect(find('h1')).to have_content 'Efren Aguirre'
   end
@@ -11,7 +15,7 @@ RSpec.describe 'Index Page' do
     expect(find('#email')).to have_content 'skeptoid@gmail.com'
   end
 
-  it "has copyright in footer'" do
+  it 'has copyright in footer' do
     expect(find('footer')).to have_content(
       'Copyright © 2014 - 2015 Efren Aguirre - Do What the Fuck You Want to
       Public License. See wtfpl for more details')
